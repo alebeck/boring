ec22a3f5ff6001635309573aa1cbefeacfcd6f06
Alexander Becker <alexander.becker@tum.de>
2024-10-20 20:27:43 +0200

    Detach process properly under Windows

--- internal/daemon/run_windows.go
+++ internal/daemon/run_windows.go
@@ -4,12 +4,19 @@ package daemon
 
 import (
 	"os/exec"
+	"syscall"
 
 	"github.com/alebeck/boring/internal/log"
 )
 
+const DETACHED_PROCESS = 0x00000008
+
 func runDaemonized(name string, arg ...string) error {
 	cmd := exec.Command(name, arg...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP | DETACHED_PROCESS,
+	}
+
 	if err := cmd.Start(); err != nil {
 		return err
 	}
