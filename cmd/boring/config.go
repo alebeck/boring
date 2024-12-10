120298a14369fab7b935eb1be1c376d9c0b05894
Alexander Becker <alexander.becker@tum.de>
2024-12-10 16:00:59 +0100

    Add default config on first use

--- cmd/boring/config.go
+++ cmd/boring/config.go
@@ -10,6 +10,22 @@ import (
 	"github.com/alebeck/boring/internal/log"
 )
 
+const defaultConfig = `# boring config file
+# An example (local) tunnel is defined below.
+# For more examples, please visit the project's GitHub page.
+# All lines starting with '#' are comments.
+
+#[[tunnels]]
+#name = "dev"  # Name for the tunnel
+#local = "localhost:9000"  # Local address to listen on
+#remote = "localhost:9000"  # Remote address to forward to
+#host = "dev-server"  # Hostname of the server, tries to match against ssh config
+#port = 22  # (Optional) Server port, defaults to 22
+#user = "joe"  # (Optional) Username, tries ssh config and defaults to $USER
+#identity = "~/.ssh/id_dev"  # (Optional) Key file, tries ssh config and defaults to default keys
+
+`
+
 func openConfig() {
 	if err := ensureConfig(); err != nil {
 		log.Fatalf("could not create config file: %v", err)
@@ -41,7 +57,10 @@ func ensureConfig() error {
 		if err != nil {
 			return err
 		}
-		f.Close()
+		defer f.Close()
+		if _, err := f.WriteString(defaultConfig); err != nil {
+			return err
+		}
 		log.Infof("Hi! Created boring config file: %s", config.Path)
 	}
 	return nil
